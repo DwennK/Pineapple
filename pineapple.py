8a521cf7acd669c5ec07ac764b65a1536a50b508
Microwest-Dwenn <Microwest-Dwenn@MICROWEST-DELL->
2023-01-12 10:33:01 +0100

    feature:working get used phone list and display it

--- pineapple.py
+++ pineapple.py
@@ -1,3 +1,18 @@
 #Get API key stored in the config.py file (that file is included in the .gitignore)
 from config import api_secret
 
+import requests
+
+url = "https://foxway.shop/api/v1/catalogs/working/pricelist"
+
+querystring = {"dimensionGroupId":"1","itemGroupId":"1","vatMargin":"true"}
+
+payload = ""
+headers = {
+    "accept": "text/json",
+    "X-ApiKey": api_secret
+}
+
+response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+print(response.text)
